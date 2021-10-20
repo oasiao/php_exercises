6ba83c957d0b7b5c69270a82c8afd154462983d8
oasiao <74007996+oasiao@users.noreply.github.com>
2021-10-21 01:07:53 +0200

    DONE but REVIEW THE DOC

--- newAgenda.php
+++ newAgenda.php
@@ -14,16 +14,19 @@ $person =[];
 $agenda = [];
 if (isset($_GET['submit'])) {
     $person = $_GET["person"]; //guardo la nueva persona
-
-    var_dump($_GET["personas"]);
     //al primer submit, $agenda no tiene nada ya que todavia no le hemos pasado los primeros valores
     //por eso da un warning
-    if ($_GET["personas"] != NULL){ //NO ME FUNCIONA
+    if ($_GET["personas"] !== NULL){ //NO ME FUNCIONA
         $agenda = $_GET["personas"]; //cogemos todos los valores anteriores
     }
 
-    //cogemos los nuevos valores y las añadimos dentro de agenda
-    $agenda[$person[0]]=$person[1];
+    if ($person[1] == ""){
+        $agenda = eliminar($agenda,$person[0]);
+    }
+    else{
+        //cogemos los nuevos valores y las añadimos dentro de agenda
+        $agenda[$person[0]]=$person[1];
+    }
 
     //muestro el formulario pasandole por parámetro la agenda
     displayForm($agenda);
@@ -44,9 +47,9 @@ function displayForm($agenda)
         <input type = "text" name = "person[]" placeholder="Name"/>
         <input type = "number" name = "person[]" placeholder="Phone"/>
         <?php
-            foreach ($agenda as $nombre => $tlf){
-                echo '<input type = "hidden" name="personas['.$nombre.']" value="'.$tlf.'"/>';
-            }
+        foreach ($agenda as $nombre => $tlf){
+            echo '<input type = "hidden" name="personas['.$nombre.']" value="'.$tlf.'"/>';
+        }
         ?>
         <input type = "submit" name="submit" value="Submit"/>
     </form>
@@ -55,10 +58,25 @@ function displayForm($agenda)
 }
 
 function output($agenda){
+
+    //TODO MEJORAR EL RESULTADO
     foreach ($agenda as $nombre => $tlf){
         echo $nombre. ":" .$tlf.'<br>';
     }
 }
+
+function eliminar($agenda, $name){
+    //TODO ELIMINAR
+    foreach ($agenda as $nombre => $tlf){
+        if ($name == $nombre){
+            unset($agenda[$nombre]);
+        }
+    }
+
+    return $agenda;
+}
+
+//EL REPLACE YA ESTA HECHO (AUTOMATICAMENTE) AL HACER KEY:VALUE
 
 ?>
 </body>
